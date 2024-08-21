dbd0b6d286a032c5db9bf0f797a5520fbbe118dd
grizeus <zaytsewww@gmail.com>
2024-08-21 12:05:34 +0300

    .

--- src/js/phone-input.js
+++ src/js/phone-input.js
@@ -1,37 +1,82 @@
 "use strict";
 
 export default (() => {
-  const phoneInput = document.querySelector('[name="telephone_number"]');
-
-  phoneInput.addEventListener("input", e => {
-    let input = e.target.value.replace(/[^\d]/g, "");
+  const form = document.querySelector("form");
+  const inputs = form.querySelectorAll("input");
+  const popups = document.querySelectorAll(".input-popup");
+  const [namePop, emailPop, telPop] = popups;
+  
+  const formatPhoneNumber = input => {
+    let value = input.value.replace(/[^\d]/g, "");
     let formatted = "";
 
-    if (input.length > 0 && input.length <= 2) {
-      formatted = `+${input}`;
-    } else if (input.length > 2 && input.length <= 5) {
-      formatted = `+${input.slice(0, 2)}-${input.slice(2)}`;
-    } else if (input.length > 5 && input.length <= 8) {
-      formatted = `+${input.slice(0, 2)}-${input.slice(2, 5)}-${input.slice(
-        5)}`;
-    } else if (input.length > 8 && input.length <= 10) {
-      formatted = `+${input.slice(0, 2)}-${input.slice(2, 5)}-${input.slice(
-        5, 8)}-${input.slice(8)}`;
-    } else if (input.length > 10 && input.length <= 12) {
-      formatted = `+${input.slice(0, 2)}-${input.slice(2, 5)}-${input.slice(
-        5, 8)}-${input.slice(8, 10)}-${input.slice(10)}`;
+    if (value.length > 0 && value.length <= 2) {
+      formatted = `+${value}`;
+    } else if (value.length > 2 && value.length <= 5) {
+      formatted = `+${value.slice(0, 2)}-${value.slice(2)}`;
+    } else if (value.length > 5 && value.length <= 8) {
+      formatted = `+${value.slice(0, 2)}-${value.slice(2, 5)}-${value.slice(
+        5
+      )}`;
+    } else if (value.length > 8 && value.length <= 10) {
+      formatted = `+${value.slice(0, 2)}-${value.slice(2, 5)}-${value.slice(
+        5,
+        8
+      )}-${value.slice(8)}`;
+    } else if (value.length > 10 && value.length <= 12) {
+      formatted = `+${value.slice(0, 2)}-${value.slice(2, 5)}-${value.slice(
+        5,
+        8
+      )}-${value.slice(8, 10)}-${value.slice(10)}`;
     }
 
-    e.target.value = formatted;
-  });
-  const popup = document.querySelector(".phone-helper-popup");
+    input.value = formatted;
+  };
 
-  phoneInput.addEventListener("invalid", e => {
-    e.preventDefault();
-    popup.style.visibility = "visible";
+  inputs.forEach(input => {
+    if (input.name === "telephone_number") {
+      input.addEventListener("input", e => formatPhoneNumber(e.target));
+    }
+
+    input.addEventListener("invalid", e => {
+      e.preventDefault();
+
+      // Reset all previous invalid marks and popup
+      inputs.forEach(inp => inp.classList.remove("input-invalid"));
+      popups.forEach(popup => (popup.style.visibility = "hidden"));
+
+      // Mark the first invalid input
+      if (!form.querySelector(".input-invalid")) {
+        input.classList.add("input-invalid");
+        switch (input.type) {
+          case "text":
+            namePop.style.visibility = "visible";
+            console.log("name");
+            break;
+          case "email":
+            emailPop.style.visibility = "visible";
+            console.log("email");
+            break;
+          case "tel":
+            telPop.style.visibility = "visible";
+            console.log("tel");
+            break;
+        }
+      }
+    });
+
+    input.addEventListener("focus", () => {
+      input.classList.remove("input-invalid");
+      popups.forEach(popup => (popup.style.visibility = "hidden"));
+    });
   });
 
-  phoneInput.addEventListener("focus", () => {
-    popup.style.visibility = "hidden";
+  form.addEventListener("submit", e => {
+    const firstInvalidInput = form.querySelector("input:invalid");
+
+    if (firstInvalidInput) {
+      e.preventDefault();
+      firstInvalidInput.focus();
+    }
   });
 })();
