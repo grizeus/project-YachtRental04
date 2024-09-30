6eee9170b331197e348e970dd1b697703dd36558
grizeus <32486511+grizeus@users.noreply.github.com>
2024-09-30 14:56:59 +0000

    Deploying to gh-pages from @ grizeus/project-YachtRental04@9224537fa8fc6e047bf13dd230f23a2cb4a58e1d 🚀

---
+++ assets/Carousel-1985d5b7.js
@@ -0,0 +1,38 @@
+import{r as a,S as r,P as i,K as l,a as t}from"./vendor-a019a59d.js";const p="_options_1b3t2_83",o="_parameter_1b3t2_94",e={"yachts-list":"_yachts-list_1b3t2_1","yachts-item":"_yachts-item_1b3t2_9","yacht-photo":"_yacht-photo_1b3t2_17","yacht-description":"_yacht-description_1b3t2_25","yacht-name":"_yacht-name_1b3t2_34","yacht-type":"_yacht-type_1b3t2_47","specs-separator":"_specs-separator_1b3t2_59","yacht-specs":"_yacht-specs_1b3t2_66","yacht-spec":"_yacht-spec_1b3t2_66",options:p,parameter:o,"yacht-rental":"_yacht-rental_1b3t2_135"},y=()=>{const[c]=a.useState(window.matchMedia("(min-width: 1280px) and (max-width: 1439px)")),[m,n]=a.useState(32);return a.useEffect(()=>{const s=()=>{n(c.matches?24:32)};return window.addEventListener("resize",s),()=>window.removeEventListener("resize",s)},[c]),React.createElement(React.Fragment,null,React.createElement(r,{modules:[i,l],slidesPerView:"auto",spaceBetween:m,pagination:{el:".swiper-pagination",clickable:!0},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},className:e["yachts-list"]},React.createElement(t,{className:e["yachts-item"]},React.createElement("picture",null,React.createElement("source",{srcSet:`
+        /images/our-yachts/14-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/14-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1440px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/13-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/13-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1280px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/12-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/12-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:768px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/11-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/11-img-yacht@2x-min.jpg 2x
+      `,media:"(max-width:767px)"}),React.createElement("img",{className:e["yacht-photo"],src:"/images/our-yachts/11-img-yacht@1x-min.jpg",alt:"yacht"})),React.createElement("div",{className:e["yacht-description"]},React.createElement("h3",{className:e["yacht-name"]},"Arrow"),React.createElement("p",{className:e["yacht-type"]},"LUXURY YAHTS")),React.createElement("hr",{className:e["specs-separator"]}),React.createElement("div",{className:e["yacht-specs"]},React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"LENGTH"),React.createElement("p",{className:e.parameter},"48m(155ft)")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"BUILDER"),React.createElement("p",{className:e.parameter},"Admiral")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"BUILT"),React.createElement("p",{className:e.parameter},"2021")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"GUESTS"),React.createElement("p",{className:e.parameter},"11 in 5 cabins")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"PRICE"),React.createElement("p",{className:e.parameter},"€33,500,000")))),React.createElement(t,{className:e["yachts-item"]},React.createElement("picture",null,React.createElement("source",{srcSet:`
+        /images/our-yachts/22-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/22-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1440px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/21-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/21-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1280px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/20-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/20-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:768px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/benetti-mob.jpg    1x,
+        /images/our-yachts/benetti-mob@2x.jpg 2x
+      `,media:"(max-width:767px)"}),React.createElement("img",{className:e["yacht-photo"],src:"/images/our-yachts/benetti-mob.jpg",alt:"yacht"})),React.createElement("div",{className:e["yacht-description"]},React.createElement("h3",{className:e["yacht-name"]},"BENETTI OASIS 40M BO122"),React.createElement("p",{className:e["yacht-type"]},"LUXURY YAHTS")),React.createElement("hr",{className:e["specs-separator"]}),React.createElement("div",{className:e["yacht-specs"]},React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"LENGTH"),React.createElement("p",{className:e.parameter},"41m (133ft)")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"SHIPYARD"),React.createElement("p",{className:e.parameter},"Benetti")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"BUILT"),React.createElement("p",{className:e.parameter},"2025")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"GUESTS"),React.createElement("p",{className:e.parameter},"10 in 5 cabins")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"PRICE"),React.createElement("p",{className:e.parameter},"€23,210,000")))),React.createElement(t,{className:e["yachts-item"]},React.createElement("picture",null,React.createElement("source",{srcSet:`
+        /images/our-yachts/31-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/31-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1440px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/30-img-yacht@1x-min.jpg 1x,
+        /images/our-yachts/30-img-yacht@2x-min.jpg 2x
+      `,media:"(min-width:1280px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/belle-anna-tab.jpg    1x,
+        /images/our-yachts/belle-anna-tab@2x.jpg 2x
+      `,media:"(min-width:768px)"}),React.createElement("source",{srcSet:`
+        /images/our-yachts/belle-anna-mob.jpg    1x,
+        /images/our-yachts/belle-anna-mob@2x.jpg 2x
+      `,media:"(max-width:767px)"}),React.createElement("img",{className:e["yacht-photo"],src:"/images/our-yachts/belle-anna-mob.jpg",alt:"yacht"})),React.createElement("div",{className:e["yacht-description"]},React.createElement("h3",{className:e["yacht-name"]},"BELLE ANNA"),React.createElement("p",{className:e["yacht-type"]},"LUXURY YAHTS")),React.createElement("hr",{className:e["specs-separator"]}),React.createElement("div",{className:e["yacht-specs"]},React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"LENGTH"),React.createElement("p",{className:e.parameter},"50m(155ft)")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"SHIPYARD"),React.createElement("p",{className:e.parameter},"ISA")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"BUILT"),React.createElement("p",{className:e.parameter},"2012")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"GUESTS"),React.createElement("p",{className:e.parameter},"12 in 6 cabins")),React.createElement("div",{className:e["yacht-spec"]},React.createElement("p",{className:e.options},"PRICE"),React.createElement("p",{className:e.parameter},"€21,000,000")))),React.createElement("div",{className:"swiper-pagination"})))};export{y as default};
+//# sourceMappingURL=Carousel-1985d5b7.js.map
