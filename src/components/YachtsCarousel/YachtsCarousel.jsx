76f439143dbb510dd40620b097d97c271e4027f5
grizeus <zaytsewww@gmail.com>
2024-10-02 16:01:08 +0300

    .

--- src/components/YachtsCarousel/YachtsCarousel.jsx
+++ src/components/YachtsCarousel/YachtsCarousel.jsx
@@ -34,210 +34,183 @@ import belleAnnaTab2x from "../../images/our-yachts/belle-anna-tab@2x.jpg";
 import belleAnnaMob from "../../images/our-yachts/belle-anna-mob.jpg";
 import belleAnnaMob2x from "../../images/our-yachts/belle-anna-mob@2x.jpg";
 
-
-const Carousel = () => {
-  // const [queryResolution] = useState(
-  //   window.matchMedia("(min-width: 1280px) and (max-width: 1439px)")
-  // );
-  // const [spaceBetween, setSpaceBetween] = useState(32);
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setSpaceBetween(queryResolution.matches ? 24 : 32);
-  //   };
-  //   window.addEventListener("resize", handleResize);
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, [queryResolution]);
-  
+const YachtsCarousel = () => {
   return (
-    <>
-      <CustomSwiper
-        mainStyle={styles["yachts-list"]} mainGap={32} secGap={24}
-      >
-        <SwiperSlide className={styles["yachts-item"]}>
-          <picture>
-            <source
-              srcSet={`
+    <CustomSwiper mainStyle={styles["yachts-list"]} mainGap={32} secGap={24}>
+      <SwiperSlide className={styles["yachts-item"]}>
+        <picture>
+          <source
+            srcSet={`
         ${yacht14x1} 1x,
         ${yacht14x2} 2x
       `}
-              media="(min-width:1440px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:1440px)"
+          />
+          <source
+            srcSet={`
         ${yacht13x1} 1x,
         ${yacht13x2} 2x
       `}
-              media="(min-width:1280px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:1280px)"
+          />
+          <source
+            srcSet={`
         ${yacht12x1} 1x,
         ${yacht12x2} 2x
       `}
-              media="(min-width:768px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:768px)"
+          />
+          <source
+            srcSet={`
         ${yacht11x1} 1x,
         ${yacht11x2} 2x
       `}
-              media="(max-width:767px)"
-            />
-            <img
-              className={styles["yacht-photo"]}
-              src={yacht11x1}
-              alt="yacht"
-            />
-          </picture>
-          <div className={styles["yacht-description"]}>
-            <h3 className={styles["yacht-name"]}>Arrow</h3>
-            <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+            media="(max-width:767px)"
+          />
+          <img className={styles["yacht-photo"]} src={yacht11x1} alt="yacht" />
+        </picture>
+        <div className={styles["yacht-description"]}>
+          <h3 className={styles["yacht-name"]}>Arrow</h3>
+          <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+        </div>
+        <hr className={styles["specs-separator"]} />
+        <div className={styles["yacht-specs"]}>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>LENGTH</p>
+            <p className={styles["parameter"]}>48m(155ft)</p>
           </div>
-          <hr className={styles["specs-separator"]} />
-          <div className={styles["yacht-specs"]}>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>LENGTH</p>
-              <p className={styles["parameter"]}>48m(155ft)</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>BUILDER</p>
-              <p className={styles["parameter"]}>Admiral</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>BUILT</p>
-              <p className={styles["parameter"]}>2021</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>GUESTS</p>
-              <p className={styles["parameter"]}>11 in 5 cabins</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>PRICE</p>
-              <p className={styles["parameter"]}>€33,500,000</p>
-            </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>BUILDER</p>
+            <p className={styles["parameter"]}>Admiral</p>
           </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles["yachts-item"]}>
-          <picture>
-            <source
-              srcSet={`
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>BUILT</p>
+            <p className={styles["parameter"]}>2021</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>GUESTS</p>
+            <p className={styles["parameter"]}>11 in 5 cabins</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>PRICE</p>
+            <p className={styles["parameter"]}>€33,500,000</p>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className={styles["yachts-item"]}>
+        <picture>
+          <source
+            srcSet={`
         ${yacht22x1} 1x,
         ${yacht22x2} 2x
       `}
-              media="(min-width:1440px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:1440px)"
+          />
+          <source
+            srcSet={`
         ${yacht21x1} 1x,
         ${yacht21x2} 2x
       `}
-              media="(min-width:1280px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:1280px)"
+          />
+          <source
+            srcSet={`
         ${yacht20x1} 1x,
         ${yacht20x2} 2x
       `}
-              media="(min-width:768px)"
-            />
-            <source
-              srcSet={`
+            media="(min-width:768px)"
+          />
+          <source
+            srcSet={`
         ${benettiMob}    1x,
         ${benettiMob2x} 2x
       `}
-              media="(max-width:767px)"
-            />
-            <img
-              className={styles["yacht-photo"]}
-              src={benettiMob}
-              alt="yacht"
-            />
-          </picture>
-          <div className={styles["yacht-description"]}>
-            <h3 className={styles["yacht-name"]}>BENETTI OASIS 40M BO122</h3>
-            <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+            media="(max-width:767px)"
+          />
+          <img className={styles["yacht-photo"]} src={benettiMob} alt="yacht" />
+        </picture>
+        <div className={styles["yacht-description"]}>
+          <h3 className={styles["yacht-name"]}>BENETTI OASIS 40M BO122</h3>
+          <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+        </div>
+        <hr className={styles["specs-separator"]} />
+        <div className={styles["yacht-specs"]}>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>LENGTH</p>
+            <p className={styles["parameter"]}>41m (133ft)</p>
           </div>
-          <hr className={styles["specs-separator"]} />
-          <div className={styles["yacht-specs"]}>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>LENGTH</p>
-              <p className={styles["parameter"]}>41m (133ft)</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>SHIPYARD</p>
-              <p className={styles["parameter"]}>Benetti</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>BUILT</p>
-              <p className={styles["parameter"]}>2025</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>GUESTS</p>
-              <p className={styles["parameter"]}>10 in 5 cabins</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>PRICE</p>
-              <p className={styles["parameter"]}>€23,210,000</p>
-            </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>SHIPYARD</p>
+            <p className={styles["parameter"]}>Benetti</p>
           </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles["yachts-item"]}>
-          <picture>
-            <source
-              srcSet={`${yacht31x1} 1x, ${yacht31x2} 2x`}
-              media="(min-width:1440px)"
-            />
-            <source
-              srcSet={`${yacht30x1} 1x, ${yacht30x2} 2x`}
-              media="(min-width:1280px)"
-            />
-            <source
-              srcSet={`${belleAnnaTab}    1x, ${belleAnnaTab2x} 2x`}
-              media="(min-width:768px)"
-            />
-            <source
-              srcSet={`${belleAnnaMob}    1x, ${belleAnnaMob2x} 2x`}
-              media="(max-width:767px)"
-            />
-            <img
-              className={styles["yacht-photo"]}
-              src={belleAnnaMob}
-              alt="yacht"
-            />
-          </picture>
-          <div className={styles["yacht-description"]}>
-            <h3 className={styles["yacht-name"]}>BELLE ANNA</h3>
-            <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>BUILT</p>
+            <p className={styles["parameter"]}>2025</p>
           </div>
-          <hr className={styles["specs-separator"]} />
-          <div className={styles["yacht-specs"]}>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>LENGTH</p>
-              <p className={styles["parameter"]}>50m(155ft)</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>SHIPYARD</p>
-              <p className={styles["parameter"]}>ISA</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>BUILT</p>
-              <p className={styles["parameter"]}>2012</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>GUESTS</p>
-              <p className={styles["parameter"]}>12 in 6 cabins</p>
-            </div>
-            <div className={styles["yacht-spec"]}>
-              <p className={styles["options"]}>PRICE</p>
-              <p className={styles["parameter"]}>€21,000,000</p>
-            </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>GUESTS</p>
+            <p className={styles["parameter"]}>10 in 5 cabins</p>
           </div>
-        </SwiperSlide>
-        {/* <div className="swiper-pagination"></div> */}
-      </CustomSwiper>
-    </>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>PRICE</p>
+            <p className={styles["parameter"]}>€23,210,000</p>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className={styles["yachts-item"]}>
+        <picture>
+          <source
+            srcSet={`${yacht31x1} 1x, ${yacht31x2} 2x`}
+            media="(min-width:1440px)"
+          />
+          <source
+            srcSet={`${yacht30x1} 1x, ${yacht30x2} 2x`}
+            media="(min-width:1280px)"
+          />
+          <source
+            srcSet={`${belleAnnaTab}    1x, ${belleAnnaTab2x} 2x`}
+            media="(min-width:768px)"
+          />
+          <source
+            srcSet={`${belleAnnaMob}    1x, ${belleAnnaMob2x} 2x`}
+            media="(max-width:767px)"
+          />
+          <img
+            className={styles["yacht-photo"]}
+            src={belleAnnaMob}
+            alt="yacht"
+          />
+        </picture>
+        <div className={styles["yacht-description"]}>
+          <h3 className={styles["yacht-name"]}>BELLE ANNA</h3>
+          <p className={styles["yacht-type"]}>LUXURY YAHTS</p>
+        </div>
+        <hr className={styles["specs-separator"]} />
+        <div className={styles["yacht-specs"]}>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>LENGTH</p>
+            <p className={styles["parameter"]}>50m(155ft)</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>SHIPYARD</p>
+            <p className={styles["parameter"]}>ISA</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>BUILT</p>
+            <p className={styles["parameter"]}>2012</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>GUESTS</p>
+            <p className={styles["parameter"]}>12 in 6 cabins</p>
+          </div>
+          <div className={styles["yacht-spec"]}>
+            <p className={styles["options"]}>PRICE</p>
+            <p className={styles["parameter"]}>€21,000,000</p>
+          </div>
+        </div>
+      </SwiperSlide>
+    </CustomSwiper>
   );
 };
 
-export default Carousel;
+export default YachtsCarousel;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React, { lazy, Suspense, StrictMode } from "react";
+import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 window.React = React;
 
-const YachtsCarousel = lazy(() => import("./components/YachtsCarousel/YachtsCarousel"));
+const YachtsCarousel = lazy(() =>
+  import("./components/YachtsCarousel/YachtsCarousel")
+);
 const ReviewsCarousel = lazy(() =>
   import("./components/ReviewsCarousel/ReviewsCarousel")
 );
@@ -10,17 +12,13 @@ const ReviewsCarousel = lazy(() =>
 import "modern-normalize";
 
 createRoot(document.getElementById("yachts-carousel")).render(
-  <StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <YachtsCarousel />
     </Suspense>
-  </StrictMode>
 );
 
 createRoot(document.getElementById("review-carousel")).render(
-  <StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <ReviewsCarousel />
     </Suspense>
-  </StrictMode>
 );
